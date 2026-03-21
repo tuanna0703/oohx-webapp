5f4b23598ae8c3e444dc7bba9d4fe57fb60b00a0
LAPTOP-S63O7FH2\tuann <vnanswer@gmail.com>
2026-03-21 16:07:12 +0700

    fix logo in Navbar and add logo for Footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,7 +26,7 @@ export default function Navbar() {
         <div className="container">
           <div className="nav-inner">
             <Link href="/" className="nav-logo">
-              <Image src="/logo_oohx.png" alt="OOHX" width={110} height={30} priority />
+              <Image src="/oohx_logo.png" alt="OOHX" width={110} height={30} priority />
             </Link>
 
             <div className="nav-links">
